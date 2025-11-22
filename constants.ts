49c8a30a174adf1723ce9b6e3880fdc193c11f88
Ratao <cassiomatsuoka@gmail.com>
2025-11-22 15:24:54 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     "id": 1,
     "title": "Casamentos",
     "category": "Wedding",
-    "imageUrl": "https://images.unsplash.com/photo-1519741497674-611481863552?auto=format&fit=crop&q=80&w=1000&sat=-100"
+    "imageUrl": "https://i.postimg.cc/DzdYHwzT/ca3cac262b434025ae4f5c9cb3e12a9d.jpg"
   },
   {
     "id": 2,
@@ -23,13 +23,13 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
     "id": 4,
     "title": "Ensaio debutante",
     "category": "Photoshoot",
-    "imageUrl": "https://images.unsplash.com/photo-1532508505999-947c2d999406?auto=format&fit=crop&q=80&w=1000"
+    "imageUrl": "https://i.postimg.cc/qRzTcKkV/1b2024318fb54bd5a9d026c8f825d066.jpg"
   },
   {
     "id": 5,
     "title": "Parto",
     "category": "Newborn",
-    "imageUrl": "https://images.unsplash.com/photo-1519689680058-324335c77eba?auto=format&fit=crop&q=80&w=1000"
+    "imageUrl": "https://i.postimg.cc/PqFGqqmr/b4f1f3718fb448589152f686230f590d.jpg"
   },
   {
     "id": 6,
@@ -40,16 +40,16 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
 ];
 
 export const GALLERY_IMAGES_SEED = [
+  "https://i.postimg.cc/0y2y4bnw/04a9b3fd7df64c229c7910a5de25f4c6.jpg",
+  "https://i.postimg.cc/qRzTcKkV/1b2024318fb54bd5a9d026c8f825d066.jpg",
+  "https://i.postimg.cc/fWcqbKnS/fda5b76d8d9445a0847893a8ff51dab0.jpg",
   "https://i.postimg.cc/JhYDxbWJ/48bf8f48b29e4f10b919865fc4b97b0f.jpg",
-  "https://images.unsplash.com/photo-1519225448526-0f09f87426fc?auto=format&fit=crop&q=80&w=800",
-  "https://i.postimg.cc/029WKw99/6389757e4f014867ab65cddfbfa8ad45.jpg",
-  "https://images.unsplash.com/photo-1542038784456-1ea0e935640e?auto=format&fit=crop&q=80&w=800",
-  "https://images.unsplash.com/photo-1551316679-9c6ae9dec224?auto=format&fit=crop&q=80&w=800",
-  "https://images.unsplash.com/photo-1604017011826-d3b4c23d891e?auto=format&fit=crop&q=80&w=800",
-  "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?auto=format&fit=crop&q=80&w=800",
-  "https://images.unsplash.com/photo-1537633552985-df8429e8048b?auto=format&fit=crop&q=80&w=800",
-  "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&q=80&w=800",
-  "https://images.unsplash.com/photo-1469334031218-e382a71b716b?auto=format&fit=crop&q=80&w=800"
+  "https://i.postimg.cc/tgJ8ZVtX/dccffcdc275b47c18dd80a094ed90d8c.jpg",
+  "https://i.postimg.cc/ZqQnp4rw/65329713ac044737924397d50236b8b5.jpg",
+  "https://i.postimg.cc/3N4qSMbQ/81a8ee51684b4b9e905d8075c1b1a3ad.jpg",
+  "https://i.postimg.cc/SKQkZs6w/de7ab51bc68e411b93cb70c0298ce063.jpg",
+  "https://i.postimg.cc/qBKVxyfw/60264c0a64254a4c9caca8ad62ad287f.jpg",
+  "https://i.postimg.cc/029WKw99/6389757e4f014867ab65cddfbfa8ad45.jpg"
 ];
 
 export const COMPANY_INFO: ContactInfo = {
